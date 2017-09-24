a56c331002dc48c2b46a3cd5311a88de1c39b9dc
Nikontos <nikontos@192.168.2.4>
2017-09-24 22:19:55 +0300

    It's working

--- Main.java
+++ Main.java
@@ -11,12 +11,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Dwsmou carbs, fats, protein \n");
-        int carbs, fats, protein;
+        int carbs, fats, protein,BG, targetBG, insulinPerU, bgPerU,carbsPerU;
+        double activeInsulin;
+
         carbs = input.nextInt();
         fats = input.nextInt();
         protein = input.nextInt();
-//        variables test = new variables(carbs,fats,protein);
-//        String kati = test.toString();
-        System.out.println(kati);
+        BG = input.nextInt();
+        targetBG = input.nextInt();
+        carbsPerU = input.nextInt();
+        bgPerU = input.nextInt();
+
+
+        activeInsulin = input.nextDouble();
+
+        variables test = new variables(BG,targetBG,carbs,fats,protein,carbsPerU,bgPerU, activeInsulin);
+        String kati = test.toString();
+        System.out.println(test.insulinCalc());
     }
 }
--- variables.java
+++ variables.java
@@ -2,13 +2,13 @@ package sample;
 
 public class variables {
 
-    private int BG;         // current blood sugar value
-    private int targetBG;   // target bg value
+    private int BG;                      // current blood sugar value
+    private int targetBG;                // target bg value
     private int carbs;
     private int fats;
     private int protein;
-    private int carbsPerU;
-    private int bgPerU;
+    private int carbsPerU;               // how many carbs a SINGLE unit of insulin can deal with
+    private int bgPerU;                  // how much your BG will drop with 1 unit of insulin
     private double activeInsulin = 0;
 
     public variables(int BG, int targetBG, int carbs, int fats, int protein, int carbsPerU, int bgPerU, double activeInsulin) {
@@ -22,36 +22,46 @@ public class variables {
         this.activeInsulin = activeInsulin;
     }
 
-    /**fsdfd**/
-
-
+    /**
+     * fsdfd
+     **/
     @Override
     public String toString() {
         return "variables{" +
-                "carbs=" + carbs +
+                "BG=" + BG +
+                ", targetBG=" + targetBG +
+                ", carbs=" + carbs +
                 ", fats=" + fats +
                 ", protein=" + protein +
+                ", carbsPerU=" + carbsPerU +
+                ", bgPerU=" + bgPerU +
+                ", activeInsulin=" + activeInsulin +
                 '}';
     }
 
-    public double insulinCalc(int carbs, int fats, int protein, boolean activeInsulin){
-        carbs = this.carbs;
-        fats = this.fats;
-        protein = this.protein;
-        activeInsulin = this.activeInsulin;
-        double dosage;
+    public double insulinCalc(){
 
-        if (activeInsulin == true) {
+        double dosage=0;
+
+        if (this.activeInsulin > 0) {
             //calculate dosage to reach target
-            int temp = targetBG - BG;
-            dosage = temp / bgPerU;
-            temp = carbs * carbsPerU;
+            int temp = this.BG - this.targetBG;        // store BG difference should be a value > 0
+            if (temp >= 0) {
+                dosage = temp / bgPerU;                // add the dosage needed to
+            }
+            temp = this.carbs / this.carbsPerU;
+            dosage += temp;
+            dosage -= this.activeInsulin;
+        }
+        else if(this.activeInsulin == 0){
+            int temp = this.BG - this.targetBG;
+            dosage = temp / this.bgPerU;
+            temp = this.carbs / this.carbsPerU;
             dosage += temp;
         }
-        
-
-
-
-        }
+        else
+            System.out.println("Line 59 Error Marie aka activeInsulin");
+        return dosage;
     }
+
 }
