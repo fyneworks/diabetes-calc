0d85ed184e503b2d5bf066d0166484e8b65003b3
Nikontos <nikontos@192.168.2.4>
2017-09-26 00:21:29 +0300

    More clarification on input when testing.

--- Main.java
+++ Main.java
@@ -11,18 +11,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Dwsmou carbs, fats, protein \n");
-        int carbs, fats, protein,BG, targetBG, insulinPerU, bgPerU,carbsPerU;
+        int carbs, fats, protein,BG, targetBG,  bgPerU,carbsPerU;
         double activeInsulin;
 
+        System.out.print("Give carbs \n");
         carbs = input.nextInt();
+        System.out.print("Gimme fats\n");
         fats = input.nextInt();
+        System.out.println("Gimme proteins");
         protein = input.nextInt();
+        System.out.print("Gimme Curent BG\n");
         BG = input.nextInt();
+        System.out.print("Gimme target BG\n");
         targetBG = input.nextInt();
+        System.out.print("Gimme Carbs per unite of insulin\n");
         carbsPerU = input.nextInt();
+        System.out.print("Gimme how much a unite of insulin lowers your BG\n");
         bgPerU = input.nextInt();
-
-
+        System.out.print("Active Insulin?\n");
         activeInsulin = input.nextDouble();
 
         variables test = new variables(BG,targetBG,carbs,fats,protein,carbsPerU,bgPerU, activeInsulin);
